b1d11b09801babdd1d08abc5feaef652e1b7c4d5
Dwebble <142026766+AlpinoMonkey@users.noreply.github.com>
2025-11-29 15:29:11 -0300

    Atualizar o bandit02_ssh.py

--- bandit02_ssh.py
+++ bandit02_ssh.py
@@ -2,7 +2,7 @@
 $ ssh bandit2@bandit.labs.overthewire.org -p 2220
 $ MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx
 $ ls -la
-$ cat "spaces in this filename"
+$ cat "./--spaces in this filename--"
 # Copy the password and paste in the Password of the exercise bandit03
 $ exit
 # Observation: the password resets all the time temporally
